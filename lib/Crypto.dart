4c6565c999e6265cda6cac7c50bfc0b75156e938
KaiBrehme <kaibrehme@t-online.de>
2023-09-17 14:11:47 +0200

    linux-enable commit

---
+++ lib/Crypto.dart
@@ -0,0 +1,17 @@
+import 'package:accordion/accordion.dart';
+import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
+
+class Crypto extends StatefulWidget {
+  const Crypto({super.key});
+
+  @override
+  State<Crypto> createState() => _CryptoState();
+}
+
+class _CryptoState extends State<Crypto> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:accordion/accordion.dart';
 import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
+import 'package:multi_tool/Crypto.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +14,41 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: Scaffold(
+            body: Accordion(header: const Text('Crypto'), children: [
+          AccordionSection(
+              header: const Text('Crypto'),
+              content: Accordion(
+                children: [
+                  AccordionSection(
+                      header: const Text('Token generator'),
+                      content: const Text(
+                          'Generate random string with the chars you want, uppercase or lowercase letters, numbers and/or symbols.')),
+                  AccordionSection(
+                      header: const Text('Hash text'),
+                      content: const Text(
+                          'Hash a text string using the function you need : MD5, SHA1, SHA256, SHA224, SHA512, SHA384, SHA3 or RIPEMD160')),
+                  AccordionSection(
+                      header: const Text('Bcrypt'),
+                      content: const Text(
+                          'Hash and compare text string using bcrypt. Bcrypt is a password-hashing function based on the Blowfish cipher.')),
+                  AccordionSection(
+                      header: const Text('UUIDs v4 generator'),
+                      content: const Text(
+                          'A Universally Unique Identifier (UUID) is a 128-bit number used to identify information in computer systems. The number of possible UUIDs is 16^32, which is 2^128 or about 3.4x10^38 (which is a lot!).')),
+                  AccordionSection(
+                      header: const Text('ULID generator'),
+                      content: const Text(
+                          'Generate random Universally Unique Lexicographically Sortable Identifier (ULID).')),
+                  AccordionSection(
+                      header: const Text('Encrypt / decrypt text'),
+                      content: const Text(
+                          'Encrypt and decrypt text clear text using crypto algorithm like AES, TripleDES, Rabbit or RC4.')),
+                ],
+              ))
+        ])),
       ),
     );
   }
